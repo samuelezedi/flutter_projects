4e56fec3a6313a64c3e1148f6446c2bf62085ffb
Samuel Ezedi <samuelezedi@gmail.com>
2020-06-22 20:32:28 +0100

    edited slider files

---
+++ lib/project.FlutterSlider/index.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SliderShow extends StatefulWidget {
+  @override
+  _SliderShowState createState() => _SliderShowState();
+}
+
+class _SliderShowState extends State<SliderShow> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
